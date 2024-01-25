281cb40d8274d50ea61981f600b9fa1f24859e07
amr <amrnooour@gmail.com>
2024-01-25 22:13:43 +0200

    added images & svgs made a file for doc app client added navigation added routing folders added colors and textstyles add empty login screen added onboarding screen

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,20 @@
+import 'package:doctor_app/core/routing/routes.dart';
+import 'package:doctor_app/features/login/ui/login_screen.dart';
+import 'package:doctor_app/features/onboarding/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter{
+  Route generateRoute(RouteSettings settings){
+    final arguments = settings.arguments;
+    switch(settings.name){
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
+      case Routes.loginScreen:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      default:
+        return MaterialPageRoute(builder: (_) => Scaffold(
+          body: Center(child: Text("No Route defined for ${settings.name}"),),
+        ));
+    }
+  }
+}
---
+++ lib/core/theming/app_assets.dart
@@ -0,0 +1,9 @@
+abstract class AppAssets{
+  static const String docdocLogo = "assets/svg/docdoc_logo.svg";
+  static const String docdocLogoLowOpacity = "assets/svg/docdoc_logo_low_opacity.svg";
+  static const String logo = "assets/svg/logo.svg";
+  static const String doctor = "assets/images/doctor.png";
+
+
+
+}
